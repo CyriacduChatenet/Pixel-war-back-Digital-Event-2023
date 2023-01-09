6a3ad6f645a2304a1f2d000101479cc2203fe82c
Cyriac du Chatenet <83903574+CyriacduChatenet@users.noreply.github.com>
2023-01-09 18:25:33 +0100

    [FIX] : fix config (#3)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -22,5 +23,64 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+=======
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { SignupUserInputDTO } from '../user/dto/signup-user.dto';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  public async validateUser(username: string, password: string) {
+    const user = await this.userService.findOneByUsername(username);
+
+    if (!user) {
+      throw new HttpException('User is not found', HttpStatus.NOT_FOUND);
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (user && isMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+  }
+
+  public async login(user: any) {
+    const payload = {
+      username: user.username,
+      password: user.password,
+      id: user.id,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  public async signup(signupUserInputDTO: SignupUserInputDTO) {
+    const user = await this.userService.findOneByUsername(
+      signupUserInputDTO.username,
+    );
+
+    if (user) {
+      throw new HttpException(
+        'User is already exist',
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+
+    const password = await bcrypt.hash(signupUserInputDTO.password, 10);
+
+    return this.userService.create({
+      ...signupUserInputDTO,
+      password,
+    });
+>>>>>>> 5ab91d0 ([FEAT] : add auth)
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { SignupUserInputDTO } from './dto/signup-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() signupUserDto: SignupUserInputDTO) {
+    return this.userService.create(signupUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Get(':username')
+  findOneByUsername(@Param('username') username: string) {
+    return this.userService.findOneByUsername(username);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() signupUserDto: SignupUserInputDTO) {
+    return this.userService.update(id, signupUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
+}
