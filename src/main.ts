6291c0aadc26c2345bf57c932638f90ee935399d
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-01-10 11:31:26 +0100

    [FEAT] : add cors and change api port

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: process.env.FRONT_URL },
+  });
+  app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
     .setTitle('Pixel War Back-end')
     .setVersion('1.0')
-    .build()
+    .build();
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
-  
-  await app.listen(3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
